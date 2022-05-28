f3bf84560dade9f796a5ad542cdc6ce69e08659e
aoyouer <whaojie06@gmail.com>
2022-05-28 19:06:45 +0000

    fix chmod

--- internal/syslog.go
+++ internal/syslog.go
@@ -15,10 +15,10 @@ func StartSysServer(addr string, onlyUnixSocket bool) (*syslog.Server, syslog.Lo
 	server.SetFormat(syslog.RFC3164)
 
 	os.Remove("/var/run/go-ngx-limiter.sock")
-	os.Chmod("/var/run/go-ngx-limiter.sock", 0766)
 	if err := server.ListenUnixgram("/var/run/go-ngx-limiter.sock"); err != nil {
 		return nil, nil, err
 	}
+	os.Chmod("/var/run/go-ngx-limiter.sock", 0766)
 	if !onlyUnixSocket {
 		if err := server.ListenTCP(addr); err != nil {
 			return nil, nil, err
