ee0958f080c501c7267dacf85afd90c9c3aef852
aoyouer <whaojie06@gmail.com>
2022-05-28 18:58:45 +0000

    socket删除与授权

--- internal/syslog.go
+++ internal/syslog.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"log"
+	"os"
 
 	"gopkg.in/mcuadros/go-syslog.v2"
 )
@@ -13,7 +14,11 @@ func StartSysServer(addr string, onlyUnixSocket bool) (*syslog.Server, syslog.Lo
 	server.SetHandler(handler)
 	server.SetFormat(syslog.RFC3164)
 
-	server.ListenUnixgram("/var/run/go-ngx-limiter.sock")
+	os.Remove("/var/run/go-ngx-limiter.sock")
+	os.Chmod("/var/run/go-ngx-limiter.sock", 0766)
+	if err := server.ListenUnixgram("/var/run/go-ngx-limiter.sock"); err != nil {
+		return nil, nil, err
+	}
 	if !onlyUnixSocket {
 		if err := server.ListenTCP(addr); err != nil {
 			return nil, nil, err
